83a7ea6efb93dbd71e6e373f43803d9ae1b395e5
Brendan Kay <bkay@mail.bradley.edu>
2021-02-23 14:36:44 -0600

    Add files via upload

--- 393/js/_vti_cnf/menu.js
+++ 393/js/_vti_cnf/menu.js
@@ -1,9 +1,10 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|DESKTOP-NN3D2O3\\drunk
 vti_modifiedby:SR|DESKTOP-NN3D2O3\\drunk
-vti_timelastmodified:TR|22 Feb 2021 20:17:23 -0000
+vti_timelastmodified:TR|23 Feb 2021 19:50:14 -0000
 vti_timecreated:TR|22 Feb 2021 20:17:23 -0000
-vti_cacheddtm:TX|22 Feb 2021 20:17:23 -0000
-vti_filesize:IR|679
 vti_extenderversion:SR|12.0.0.0
 vti_backlinkinfo:VX|393/Adventures.html 393/BUAC.html
+vti_nexttolasttimemodified:TW|23 Feb 2021 19:27:35 -0000
+vti_cacheddtm:TX|23 Feb 2021 19:50:14 -0000
+vti_filesize:IR|1453
